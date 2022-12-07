87eee0695397a63ae32342ce537ddce9fe30e548
Ranieri-Vahl <devranieri@gmail.com>
2022-12-07 12:25:54 -0300

    Starting the site of recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
-def contato(request):
-    return HttpResponse('contato')
-
-
-def sobre(request):
-    return HttpResponse('SOBRE O INTER')
+    return render(request, 'recipes/pages/home.html')
 
