9a978a87eabec897412e3fcb60982dcfb1c3e723
Ranieri-Vahl <devranieri@gmail.com>
2022-12-16 12:14:11 -0300

    working in the tests

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import get_list_or_404, render, get_object_or_404
 
-from utils.recipes.factory import make_recipe
-
 from .models import Recipe
 
 # Create your views here.
@@ -21,7 +19,7 @@ def category(request, category_id):
         category__id=category_id,
         is_published=True
         ).order_by('-id'))
-
+    
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
         'title': f'{recipes[0].category.name} - Category 〡'
