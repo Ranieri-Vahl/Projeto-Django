b41f8ff9d3473cd68b7dea0a2c958409ea17e656
Ranieri-Vahl <devranieri@gmail.com>
2023-02-09 10:41:02 -0300

    Resizing Images

--- recipes/models.py
+++ recipes/models.py
@@ -1,9 +1,12 @@
+import os
 from collections import defaultdict
 
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
 from django.forms import ValidationError
 from django.utils.text import slugify
+from PIL import Image
 
 from tag.models import Tag
 
@@ -42,13 +45,40 @@ class Recipe(models.Model):
     def __str__(self):
         return self.title
 
+    @staticmethod
+    def resize_image(image, new_width=800):
+        image_full_path = os.path.join(settings.MEDIA_ROOT, image.name)
+        image_pillow = Image.open(image_full_path)
+        original_width, original_height = image_pillow.size
+
+        if original_width <= new_width:
+            image_pillow.close()
+            return
+
+        new_height = round((new_width * original_height) / original_width)
+
+        new_image = image_pillow.resize((new_width, new_height), Image.LANCZOS)
+        new_image.save(
+            image_full_path,
+            optimize=True,
+            quality=50,
+        )
+
     def save(self, *args, **kwargs):
         if not self.slug:
             slug = f'{slugify(self.title)}'
             self.slug = slug
 
-        return super().save(*args, **kwargs)
-    
+        saved = super().save(*args, **kwargs)
+
+        if self.cover:
+            try:
+                self.resize_image(self.cover, 800)
+            except FileNotFoundError:
+                ...           
+
+        return saved
+
     def clean(self, *args, **kwargs):
         error_messages = defaultdict(list)
 
